4a10b495bf9a10565803501ab47702ab1c75cb5b
Alexey Bondarenko <alexeybondarenko@me.com>
2015-09-20 14:55:34 +0300

    Initial Commit

---
+++ OpenGL Lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL Lab1
+//
+//  Created by Alexey Bondarenko on 9/20/15.
+//  Copyright (c) 2015 Alexey Bondarenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
